635d7eec6b0df135adca1d18c84c887d8675b561
Percy Salgado <percysalgado@icloud.com>
2017-08-23 21:57:03 -0500

    Lista software instalado y lo envia al Escritorio

---
+++ apps/listado de softwares instalados.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Nombre del paquete 		:	CFBundleName
+# Ejecutable			:	CFBundleExecutable
+# Versión del paquete		:	CFBundleShortVersionString
+# Fabricante 			:	NSHumanReadableCopyright
+# Fabricante (otros)		:	CFBundleGetInfoString
+
+
+for i in /Applications/*app
+	do
+	app=$(/usr/libexec/plistbuddy -c Print:CFBundleName: "$i"/Contents/info.plist)
+    	version=$(/usr/libexec/plistbuddy -c Print:CFBundleShortVersionString: "$i"/Contents/info.plist)
+    	vendor=$(/usr/libexec/plistbuddy -c Print:NSHumanReadableCopyright: "$i"/Contents/info.plist)
+    	echo "$app","$version","$vendor" >> ~/Desktop/listado.csv
+    done
+    
+for i in /Applications/*/*app
+	do
+	app=$(/usr/libexec/plistbuddy -c Print:CFBundleName: "$i"/Contents/info.plist)
+    	version=$(/usr/libexec/plistbuddy -c Print:CFBundleShortVersionString: "$i"/Contents/info.plist)
+    	vendor=$(/usr/libexec/plistbuddy -c Print:NSHumanReadableCopyright: "$i"/Contents/info.plist)
+    	echo "$app","$version","$vendor" >> ~/Desktop/listado.csv
+    done
+    
